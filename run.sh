491a21fbe4a7f6cf560039a735a020fd38a5f0a3
maksim <you@example.com>
2025-05-13 01:05:37 +1000

    Оптимизировал образ

--- run.sh
+++ run.sh
@@ -5,18 +5,18 @@ case $COMMAND in
     crud_api)
         echo "Запускается сервис crud_api..."
         cd crud_api
-        uv run alembic upgrade head
+        alembic upgrade head
         python cli.py filldb
-        uv run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+        uvicorn main:app --host 0.0.0.0 --port 8000 --reload
         ;;
     admin)
         cd admin
-        uv run streamlit run main.py --client.showErrorDetails=false
+        streamlit run main.py --client.showErrorDetails=false
        ;;
     solve_api)
         echo "Запускается сервис solve_api..."
         cd solve_api
-        uv run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+        uvicorn main:app --host 0.0.0.0 --port 8000 --reload
         ;;
     *)
         echo "Ошибка: Неверный параметр '$COMMAND'. Используйте 'crud_api' или 'solve_api' или 'admin'"
